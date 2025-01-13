72e507c237222fd99c650dd27b67c0c18aaaf434
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-13 23:04:23 +0200

    add function

--- Basic/3.List and Array/0.Doers/Doers.py
+++ Basic/3.List and Array/0.Doers/Doers.py
@@ -1,4 +1,5 @@
 import os
+import random
 from typing import Union, Any
 
 
@@ -27,18 +28,72 @@ def display_opt():
 
 
 def add_item(tasks) -> list:
-    pass
+    def set_priority(task) -> str:
+        levels = ["emergency_pos","high", "medium", "low"]
+
+        for idx,level in enumerate(levels,start=1):
+            print(f"[{idx}]. {level}")
+
+        priority = input("Enter Priority [Number/Description]: ")
+
+        if isinstance(priority,str) and priority.lower() in levels:
+            return priority
+        elif isinstance(priority,int) and priority in range(1,len(levels)):
+            return levels[priority-1]
+
+
+    def inserting_task(tasks,task,importance) -> list:
+        def override(lst,act,pos) -> list:
+            check = input("Would you like to Override this task? [y/n]: ").lower()
+            if check[0] not in ["y","n"] or check[0] == 'y':
+                lst.insert(pos,act)
+            return lst
+
+        position = 0
+
+        if importance == "emergency_pos":
+            if tasks[0] != " ":
+                print("Emergency Slot Taken")
+                position = 0
+        else:
+            if importance == "high":
+                position = [i for i in range(1, 4)]
+            elif importance == "medium":
+                position = [i for i in range(1, 4)]
+            else:
+                position = [i for i in range(7, 11)]
+
+
+
+
+        tasks = override(tasks, task, position)
+        return tasks
+
+    try:
+        if len(tasks)>10:
+            raise IndexError
+
+        task = input("Enter Task: ")
+        priority = set_priority(task)
+        tasks = inserting_task(tasks,task,priority)
+
+        return tasks
+    except IndexError:
+        print("Tasks full - Complete a few tasks")
+        return tasks
 
 
 def remove_item(tasks) -> list:
     pass
+
 
 def show_item(tasks):
     pass
 
 
 def main():
-    tasks = []
+    tasks = [" " for _ in range(10)]
+    print(tasks)
     clear_terminal()
     print("__Task Manager__")
 
