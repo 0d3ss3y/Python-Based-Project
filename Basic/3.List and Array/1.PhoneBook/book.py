a0e238b645bd9471ca1549086b88fa331eac6d31
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-14 09:07:52 +0200

    Display Option Completed

--- Basic/3.List and Array/1.PhoneBook/book.py
+++ Basic/3.List and Array/1.PhoneBook/book.py
@@ -10,8 +10,31 @@ def adding_contact():
     pass
 
 
+def display_opt():
+    try:
+        options = ["Add", "Search", "Sort", "Edit", "Delete", "Display"]
+        print("\n__Options__:")
+
+        for key, option in enumerate(options, start=1):
+            print(f"[{key}]. {option}]")
+
+        opt = int(input("\nEnter your option [1-6]: "))
+
+        if 1 > opt > 6:
+            raise ValueError("Illegal input")
+        else:
+            return options[opt-1]
+
+    except ValueError as error:
+        print(f"Error 404 - Not Found: {error}")
+
+
 def main():
-    pass
+    print("__Contact Book__")
+
+    while True:
+        opt = display_opt()
+
 
 if __name__ == '__main__':
     main()
