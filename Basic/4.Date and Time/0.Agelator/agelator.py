fba07439f2aa5f1504f78b4152624a5f87bb54dd
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-17 23:46:16 +0200

    Retrieve Info

--- Basic/4.Date and Time/0.Agelator/agelator.py
+++ Basic/4.Date and Time/0.Agelator/agelator.py
@@ -11,7 +11,58 @@ def determine_age(birthday):
 
 
 def determine_zodiac(birthday):
-    pass
+    chart = {
+        "Aries" : {
+            "start" : "3/21",
+            "end" : "4/19"
+        },
+        "Taurus" : {
+            "start": "4/20",
+            "end": "5/20"
+        },
+        "Gemini": {
+            "start": "5/21",
+            "end": "6/20"
+        },
+        "Cancer": {
+            "start": "6/21",
+            "end": "7/22"
+        },
+        "Leo": {
+            "start": "7/23",
+            "end": "8/22"
+        },
+        "Virgo": {
+            "start": "8/23",
+            "end": "9/22"
+        },
+        "Libra": {
+            "start": "9/23",
+            "end": "10/22"
+        },
+        "Scorpio": {
+            "start": "10/22",
+            "end": "11/21"
+        },
+        "Sagittarius": {
+            "start": "11/22",
+            "end": "12/21"
+        },
+        "Capricorn": {
+            "start": "12/22",
+            "end": "1/19"
+        },
+        "Aquarius": {
+            "start": "1/20",
+            "end": "2/18"
+        },
+        "Pisces": {
+            "start": "2/19",
+            "end": "3/20"
+        }
+    }
+
+
 
 
 def option() -> Union[str,None]:
@@ -32,7 +83,35 @@ def option() -> Union[str,None]:
 
 
 def retrieve_birthday():
-    pass
+    def check_format(placeholder) -> bool:
+        valid = False
+        try:
+            if "/" not in placeholder:
+                raise ValueError(f"invalid Date: {placeholder}")
+            else:
+                year, month, day = placeholder.split("/")
+
+                if len(year) == 4 and len(month) == 2 and (len(day) == 2 or len(day) == 1):
+                    valid = True
+                return valid
+
+        except (ValueError,TypeError) as error:
+            print(f"Error 404 - Not Found: {error}")
+            return valid
+
+    try:
+        birthday = input("Enter your birthday [yyyy/mm/dd]: ")
+        flag = check_format(birthday)
+
+        if flag:
+            year, month, day = birthday.split("/")
+            return year, month, day
+        else:
+            raise ValueError(f"Invalid Date: {birthday}")
+
+    except ValueError as error:
+        print(f"Error 404 - Not Found: {error}")
+        return None
 
 
 def main():
