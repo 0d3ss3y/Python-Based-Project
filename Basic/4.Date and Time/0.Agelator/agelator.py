0c054d2680b04daedb5c18285242df1871dcca16
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-17 21:30:58 +0200

    Options Up

--- Basic/4.Date and Time/0.Agelator/agelator.py
+++ Basic/4.Date and Time/0.Agelator/agelator.py
@@ -1,3 +1,7 @@
+import datetime
+from typing import Union
+
+
 def find_next_birthday_day(birthday):
     pass
 
@@ -10,8 +14,21 @@ def determine_zodiac(birthday):
     pass
 
 
-def option():
-    pass
+def option() -> Union[str,None]:
+    try:
+        options = ["Determine Age", "Determine Zodiac", "Determine Next Birthday"]
+        print("\n__Options__")
+        for key, opt in enumerate(options, start=1):
+            print(f"[{key}] {opt}")
+
+        selection = int(input(f"Enter your selection [1-{len(options)}]: "))
+        if 1 <= selection <= len(options):
+            return options[selection - 1]
+        else:
+            raise ValueError("Selection out of range.")
+    except ValueError as error:
+        print(f"Invalid input: {error}")
+        return None
 
 
 def retrieve_birthday():
