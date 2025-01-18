84ffac72a344134187fb2246afe43689db2efedf
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-18 16:41:30 +0200

    Option Up

--- Basic/5.File Handling/0.Notifer/notify.py
+++ Basic/5.File Handling/0.Notifer/notify.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 def load_notes():
     pass
 
@@ -18,13 +21,22 @@ def delete_notes():
     pass
 
 
-def option():
-    options = ["Load Notes", "Download Notes", "Save Notes", "Create Notes", "Delete Notes"]
+def option() -> Union[str,None]:
+    try:
+        options = ["Load Notes", "Download Notes", "Save Notes", "Create Notes", "Delete Notes"]
 
-    for key,opt in enumerate(options):
-        print(f"[{key}]. {opt}")
+        for key, opt in enumerate(options,start=1):
+            print(f"[{key}]. {opt}")
 
+        selection = int(input(f"Select an option [1-{len(options)}]: "))
 
+        if 1 > selection > len(options):
+            raise ValueError("Invalid selection")
+        return options[selection - 1]
+
+    except ValueError as error:
+        print(f"Error 404 - {error}")
+        return None
 
 
 def main():
