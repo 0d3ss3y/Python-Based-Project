e6846c50c8f09fe58599b3045898f8580189c73c
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-18 16:30:40 +0200

    Spine Up

---
+++ Basic/5.File Handling/0.Notifer/notify.py
@@ -0,0 +1,26 @@
+def load_notes():
+    pass
+
+
+def download_notes():
+    pass
+
+
+def save_notes():
+    pass
+
+
+def create_notes():
+    pass
+
+
+def delete_notes():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
