e18d1883285d8cf944ad606eb92e03084afa4c05
0d3ss3y <samuelkhoali002@gmail.com>
2025-01-18 17:59:54 +0200

    Main Function Up

--- Basic/5.File Handling/0.Notifer/notify.py
+++ Basic/5.File Handling/0.Notifer/notify.py
@@ -1,4 +1,10 @@
+import os
+import sys
 from typing import Union
+
+
+def clear_terminal():
+    return os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def load_notes():
@@ -21,8 +27,13 @@ def delete_notes():
     pass
 
 
+def edit_notes():
+    pass
+
+
 def option() -> Union[str,None]:
     try:
+        print("\n__Options__:")
         options = ["Load Notes", "Download Notes", "Save Notes", "Create Notes", "Delete Notes"]
 
         for key, opt in enumerate(options,start=1):
@@ -41,6 +52,35 @@ def option() -> Union[str,None]:
 
 def main():
     print("__Note Maker__\n")
+
+    try:
+        notes = {}
+        while True:
+            opt = option()
+
+            if opt == "Load Notes":
+                notes = load_notes()
+            elif opt == "Download Notes":
+                download_notes(notes)
+                print("Download Complete")
+            elif opt == "Save Notes":
+                save_notes(notes)
+                print("Notes successfully saved")
+            elif opt == "Create Notes":
+                notes = create_notes(notes)
+                print("Notes successfully created")
+            elif opt == "Delete Notes":
+                notes = delete_notes(notes)
+                print("Notes successfully deleted")
+            elif opt == "Edit Notes":
+                notes = edit_notes(notes)
+            elif opt == "Exit":
+                raise SystemExit(0)
+
+    except (KeyboardInterrupt, SystemExit):
+        print("\nExiting...")
+        sys.exit()
+
 
 
 if __name__ == '__main__':
